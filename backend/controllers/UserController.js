778cf3d7c20d29d265cccefd383a05da6462d91b
Bryan Caldera <bryanicaldera@gmail.com>
2022-11-22 21:14:11 -0800

    user routes added

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,7 +3,10 @@ const db = require("../db_connection");
 exports.getByID = (req, res) => {
 
     const id = req.query.id
-    let sql = "SELECT * FROM employees WHERE emp_no = ?"
+    let sql = "SELECT employees.*, titles.title, salary FROM salaries \
+    JOIN employees ON salaries.emp_no = employees.emp_no \
+    JOIN titles ON titles.emp_no = employees.emp_no \
+    WHERE salaries.emp_no = ? ORDER BY salaries.to_date DESC, titles.to_date DESC LIMIT 1"
 
     db.query(sql, [id], (err, results) => {
         if (err) {
@@ -12,7 +15,7 @@ exports.getByID = (req, res) => {
                 message: err
             })
         }
-        if(results.length === 0) {
+        if (results.length === 0) {
             return res.status(404).send({
                 status: "error",
                 message: "No user found"
@@ -20,7 +23,7 @@ exports.getByID = (req, res) => {
         }
         return res.status(200).send({
             status: "success",
-            results: results                    
+            results: results
         })
     })
 
@@ -28,7 +31,7 @@ exports.getByID = (req, res) => {
 
 exports.getEmployeeCount = (req, res) => {
 
-    let sql = "SELECT COUNT(*) FROM employees"
+    let sql = "SELECT COUNT(*) AS EmpCount FROM employees"
 
     db.query(sql, (err, results, fields) => {
         if (err) {
@@ -45,3 +48,126 @@ exports.getEmployeeCount = (req, res) => {
     })
 
 }
+
+exports.editByID = (req, res) => {
+
+    const id = req.query.id
+    const first_name = req.query.first_name
+    const last_name = req.query.last_name
+    const gender = req.query.gender
+
+    let sql = "UPDATE employees SET first_name = ?, last_name = ?, gender = ?  WHERE emp_no = ?"
+
+    db.query
+        (
+            sql,
+            [first_name, last_name, gender, id],
+            (err, results) => {
+                if (err) {
+                    return res.status(401).send({
+                        status: "error",
+                        message: err
+                    })
+                }
+                if (results.affectedRows === 0) {
+                    return res.status(404).send({
+                        status: "error",
+                        message: "No user found"
+                    })
+                }
+                return res.status(200).send({
+                    status: "success",
+                    results: results
+                })
+            }
+        )
+}
+
+
+exports.getByDepartment = (req, res) => {
+
+    const dept = req.query.dept
+    const count = parseInt(req.query.count)
+    const offset = parseInt(req.query.offset)
+
+    let sql = "SELECT employees.emp_no, first_name, last_name FROM employees \
+    JOIN current_dept_emp ON current_dept_emp.emp_no = employees.emp_no \
+    WHERE dept_no = ? ORDER BY EMP_NO DESC LIMIT ? OFFSET ?"
+
+    db.query(sql, [dept, count, offset], (err, results) => {
+        if (err) {
+            return res.status(401).send({
+                status: "error",
+                message: err
+            })
+        }
+        if (results.length === 0) {
+            return res.status(404).send({
+                status: "error",
+                message: "No employees found"
+            })
+        }
+        return res.status(200).send({
+            status: "success",
+            results: results
+        })
+    }
+    )
+}
+
+exports.getDepartmentsManagers = (req, res) => {
+
+    let sql = "SELECT departments.dept_no, departments.dept_name, employees.first_name, employees.last_name, employees.emp_no \
+    FROM departments \
+    JOIN dept_manager ON dept_manager.dept_no = departments.dept_no \
+    JOIN employees ON employees.emp_no = dept_manager.emp_no \
+    WHERE dept_manager.to_date = '9999-01-01'"
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(401).send({
+                status: "error",
+                message: err
+            })
+        }
+        if (results.length === 0) {
+            return res.status(404).send({
+                status: "error",
+                message: "No departments found"
+            })
+        }
+        return res.status(200).send({
+            status: "success",
+            results: results
+        })
+    })
+}
+
+exports.getDepartments = (req, res) => {
+
+    let sql = "SELECT * FROM departments"
+    
+    db.query(sql, (err ,results) => {
+        if (err) {
+            return res.status(401).send({
+                status: "error",
+                message: err
+            })
+        }
+        if (results.length === 0) {
+            return res.status(404).send({
+                status: "error",
+                message: "No departments found"
+            })
+        }
+        return res.status(200).send({
+            status: "success",
+            results: results
+        })
+    })
+}
+
+
+//get managers 
+//get salaries
+//get titles
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {getEmployeeCount, getByID} = require("../controllers/UserController");
+const {getEmployeeCount, getByID, getByDepartment, getDepartments, editByID} = require("../controllers/UserController");
 
 router.get("/getEmployeeCount", getEmployeeCount);
 router.get("/getByID", getByID);
+router.get("/getByDepartment", getByDepartment);
 
 module.exports = router;
