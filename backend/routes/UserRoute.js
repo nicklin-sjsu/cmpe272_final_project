ee6f5f5fc42399764709c98c43c02433b9cbb93a
Bryan Caldera <bryanicaldera@gmail.com>
2022-11-22 21:18:23 -0800

    missing routing

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getEmployeeCount, getByID, getByDepartment, getDepartments, editByID} = require("../controllers/UserController");
+const {getEmployeeCount, getByID, getByDepartment, getDepartments, editByID, getDepartmentsManagers} = require("../controllers/UserController");
 
 router.get("/getEmployeeCount", getEmployeeCount);
 router.get("/getByID", getByID);
 router.get("/getByDepartment", getByDepartment);
-
+router.get("/getDepartments", getDepartments);
+router.get("/getDepartmentsManagers", getDepartmentsManagers);
+router.get("/editByID", editByID);
 module.exports = router;
