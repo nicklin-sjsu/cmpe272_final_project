0f1cfafa9c459eac5a9869c10cccb408ae048369
Bryan Caldera <bryanicaldera@gmail.com>
2022-11-24 01:17:09 -0800

    bad import

--- backend/routes/AdminRoute.js
+++ backend/routes/AdminRoute.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {getAllEmployees, getByID, editByID, deleteByID, editSalary, editTitle, getAllEmployeesCurrent} = require("../controllers/AdminController");
+const {getAllEmployees, getByID, editByID, deleteByID, editSalary, getAllEmployeesCurrent} = require("../controllers/AdminController");
 
 router.get("/getAllEmployees", getAllEmployees);
 router.get("/getByID", getByID);
 router.get("/editByID", editByID);
 router.get("/deleteByID", deleteByID);
 router.get("/editSalary", editSalary);
-router.get("/editTitle", editTitle);
 router.get("/getAllEmployeesCurrent", getAllEmployeesCurrent);
 
 module.exports = router;
