8b5505c714d3a4846dc390db7361d5c09f50594a
Bryan Caldera <bryanicaldera@gmail.com>
2022-11-26 02:50:56 -0800

    search by name improved

--- backend/controllers/SearchController.js
+++ backend/controllers/SearchController.js
@@ -1,4 +1,5 @@
 const db = require("../db_connection");
+const levenshtein = require('fast-levenshtein');
 
 exports.searchName = (req, res) => {
     const name = req.query.name;
@@ -9,7 +10,13 @@ exports.searchName = (req, res) => {
             message: "Please enter a name"
         })
     }
-    let sql = `SELECT * FROM employees WHERE `;
+
+    let sql = `SELECT employees.emp_no, CONCAT(first_name, ' ', last_name) AS Name, titles.title, departments.dept_name FROM employees \
+    JOIN titles ON titles.emp_no = employees.emp_no \
+    JOIN dept_emp ON dept_emp.emp_no = employees.emp_no \
+    JOIN departments ON departments.dept_no = dept_emp.dept_no \
+    WHERE `;
+
     for (let i = 0; i < words.length; i++) {
         if (i === words.length - 1) {
             sql += `first_name LIKE '%${words[i]}%' OR last_name LIKE '%${words[i]}%'`
@@ -17,6 +24,9 @@ exports.searchName = (req, res) => {
             sql += `first_name LIKE '%${words[i]}%' OR last_name LIKE '%${words[i]}%' OR `
         }
     }
+
+    sql += " GROUP BY employees.emp_no LIMIT 10000" // LIMIT 10000 to prevent server from crashing
+   
     db.query(sql, (err, results) => {
         if (err) {
             return res.status(401).send({
@@ -30,9 +40,26 @@ exports.searchName = (req, res) => {
                 message: "No results found"
             })
         }
+
+        let message = "success";
+        if (results.length === 10000) {
+            message = "Returned more than 10,000 results. Please refine your search for more accurate results."
+        }
+
+        //use levenshtein distance to find closest match to original search
+        let searchResults = results
+            .map((result) => {
+                return {
+                    ...result,
+                    distance: levenshtein.get(name, result.Name)
+                }
+            })
+            .sort((a, b) => a.distance - b.distance)
+            .slice(0, 100); // only return top 100 results
+
         return res.status(200).send({
-            status: "success",
-            results: results
+            status: message,
+            results: searchResults
         })
     }
     )
