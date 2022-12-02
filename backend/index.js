5a2d222457ea349fdb45ea9a116e41fb2d50e386
Bryan Caldera <bryanicaldera@gmail.com>
2022-12-02 04:15:14 -0800

    chrome is evil

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", req.header('origin'));
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Private-Network", "true")
     next();
 });
 
